31a0fda8484ab77d21761c03a82372263518c314
Evandro Pijanowski <evandro.pijanowski@icloud.com>
2026-08-13 17:33:32 -0300

    test: strenghten web product validations

--- cypress/pages/CartPage.js
+++ cypress/pages/CartPage.js
@@ -1,11 +1,31 @@
 class CartPage {
   assertProduct(product) {
     cy.get("#cart_items").within(() => {
-      cy.contains("tbody tr", product.name).should("be.visible").within(() => {
-        cy.get(".cart_description").should("contain.text", product.name);
-        cy.get(".cart_price").should("contain.text", product.price);
+    cy.contains("tbody tr", product.name)
+      .should("be.visible")
+      .within(() => {
+        cy.get(".cart_description")
+          .should("contain.text", product.name);
+
+        cy.get(".cart_price")
+          .invoke("text")
+          .then((price) => {
+            expect(price.trim()).to.eq(product.price.trim());
+          });
+
+        cy.get(".cart_quantity button")
+          .invoke("text")
+          .then((quantity) => {
+            expect(quantity.trim()).to.eq("1");
+          });
+
+        cy.get(".cart_total_price")
+          .invoke("text")
+          .then((total) => {
+            expect(total.trim()).to.eq(product.price.trim());
+          });
       });
-    });
+  });
   }
 
   removeProduct(productName) {
